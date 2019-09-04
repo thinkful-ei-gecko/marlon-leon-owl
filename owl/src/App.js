f17095360217830fbc08612c98119428efb2fafc
Leon Dail <leondailofficial@gmail.com>
2019-09-04 11:39:23 -0700

    basic flex styling

--- owl/src/App.js
+++ owl/src/App.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+        <section className='mainContainer'>
+            <div className='stageLeft'>
+                <div className='topBar left'>
+                    <ul>
+                      <li>Chat</li>
+                      <li>Participants (2)</li>
+                      <li>Buttons</li>
+                    </ul>
+                </div>
+                <div className='chatLog'>
+                
+                </div>
+            </div>
+            <div className='stageRight'>
+                <div className='topBar right'>
+                    <ul>
+                        <li>Left Arrow Button</li>
+                        <li>Stage</li>
+                        <li>Leon's Screen X</li>
+                    </ul>
+                </div>
+                <div className='stageLog'>
+
+                </div>
+            </div>
+        </section>
+        <section className='bottomFooter'>
+          <p>Test data 2 ---- fillllllll </p>
+        </section>
     </main>
   );
 }
